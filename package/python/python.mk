d643deab8ea095367b128eda1a639bbac032a138
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2017-03-08 00:00:27 +0100

    python: make hashlib and readline modules really optional
    The BR2_PACKAGE_PYTHON_{READLINE,HASHLIB} options were so far only
    bringing in the necessary dependencies, relying on the Python build
    system to automatically detect them.
    
    However, this means that even if one of those option was disabled, if
    their dependency was found, Python would build the corresponding module,
    which is really not what the user would expect.
    
    For example, if you have:
    
       BR2_PACKAGE_READLINE=y
       # BR2_PACKAGE_PYTHON_READLINE is not set
    
    Then you would still get the readline Python module built and installed.
    
    This commit fixes that by adding new --{enable,disable} options, and use
    them in python.mk.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Reviewed-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    (cherry picked from commit bc3e1fb22ef641916938fff76bef10a1347ef0f7)
    
    Change-Id: I9d1af53ab137a3dd1ac6cdd850bb8df24aceeda4
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/python/python.mk
+++ package/python/python.mk
@@ -58,6 +58,8 @@ PYTHON_INSTALL_STAGING = YES
 
 ifeq ($(BR2_PACKAGE_PYTHON_READLINE),y)
 PYTHON_DEPENDENCIES += readline
+else
+PYTHON_CONF_OPTS += --disable-readline
 endif
 
 ifeq ($(BR2_PACKAGE_PYTHON_CURSES),y)
@@ -118,6 +120,8 @@ endif
 
 ifeq ($(BR2_PACKAGE_PYTHON_HASHLIB),y)
 PYTHON_DEPENDENCIES += openssl
+else
+PYTHON_CONF_OPTS += --disable-hashlib
 endif
 
 ifeq ($(BR2_PACKAGE_PYTHON_OSSAUDIODEV),y)
