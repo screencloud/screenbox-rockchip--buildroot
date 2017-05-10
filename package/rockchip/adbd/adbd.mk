1412cdd28ce0c522b5e4cc29eddb66669489b30f
cww <cww@rock-chips.com>
2017-05-10 09:47:04 +0800

    fix package/rockchip release version build error.
    Change-Id: I445115873e075780a2ae401c5d8985bcfe478b87
    Signed-off-by: cww <cww@rock-chips.com>

--- package/rockchip/adbd/adbd.mk
+++ package/rockchip/adbd/adbd.mk
@@ -1,11 +1,16 @@
 # Rockchip's adbd porting for Linux
 # Author : Cody Xie <cody.xie@rock-chips.com>
 
+ifeq ($(BR2_PACKAGE_RK3036_ECHO),y)
+ADBD_SITE = $(TOPDIR)/../external/adb
+ADBD_SITE_METHOD = local
+else
 ADBD_SITE = $(call qstrip, ssh://git@10.10.10.78:2222/argus/externals/adb.git)
 ADBD_SITE_METHOD = git
-ADBD_VERSION = 0d8ec12
 ADBD_SOURCE = adbd-${ADBD_VERSION}.tar.gz
 ADBD_FROM_GIT = y
+endif
+ADBD_VERSION = 0d8ec12
 
 ADBD_DEPENDENCIES += libcutils
 
