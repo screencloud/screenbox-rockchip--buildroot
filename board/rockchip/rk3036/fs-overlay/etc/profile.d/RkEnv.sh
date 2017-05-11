047d28f255f186e29d1d46100db879268d27926d
cww <cww@rock-chips.com>
2017-05-11 20:02:29 +0800

    rk3036: echo: config data partition readonly.
    Change-Id: I8251abaf9ff6d9438884ecf51804e93bba44c9f6
    Signed-off-by: cww <cww@rock-chips.com>

--- board/rockchip/rk3036/fs-overlay/etc/profile.d/RkEnv.sh
+++ board/rockchip/rk3036/fs-overlay/etc/profile.d/RkEnv.sh
@@ -1,6 +1,6 @@
-export HOME=/data
-export PATH=$PATH:$HOME:$HOME/bin
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/usrlibs
+export HOME=/data/cfg
+export PATH=$PATH:$HOME:$HOME/bin:/data/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/data/usrlibs
 
 # auto run /data/RkLunch.sh
 source /data/RkLunch.sh
