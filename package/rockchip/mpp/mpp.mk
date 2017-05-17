588f2406267450dafdd898cab241d8eec121f2ec
Randy Li <randy.li@rock-chips.com>
2017-05-17 11:21:32 +0800

    rockchip-bsp: mpp: add an option for drm allocator
    Some platforms still use ion allocator and they use the same
    meta data.
    
    I have to add an option for this purpose.
    
    Change-Id: Ie052ed7505dfd93ea755d95e6e70871f904a65e9
    Signed-off-by: Randy Li <randy.li@rock-chips.com>

--- package/rockchip/mpp/mpp.mk
+++ package/rockchip/mpp/mpp.mk
@@ -3,7 +3,12 @@
 MPP_SITE = "git://github.com/rockchip-linux/mpp.git"
 MPP_VERSION = for_linux
 MPP_CONF_OPTS = "-DRKPLATFORM=ON"
+MPP_CONF_DEPENDENCIES += libdrm
 
 MPP_INSTALL_STAGING = YES
+
+ifeq ($(BR2_PACKAGE_MPP_ALLOCATOR_DRM),y)
+MPP_CONF_OPTS += "-DHAVE_DRM=ON"
+endif
 
 $(eval $(cmake-package))
