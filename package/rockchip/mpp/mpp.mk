55fbcf206b02250f0ad737d82160cffe29e1a737
hero.huang <hero.huang@rock-chips.com>
2017-07-06 09:38:19 +0800

    rockchip-bsp: mpp: rename for_linux as release
    Change-Id: I59170ddfecabd9e94ba7e0e58250bbd79f56a8ed
    Signed-off-by: hero.huang <hero.huang@rock-chips.com>

--- package/rockchip/mpp/mpp.mk
+++ package/rockchip/mpp/mpp.mk
@@ -1,7 +1,7 @@
 # Rockchip's MPP(Multimedia Processing Platform)
 
 MPP_SITE = "git://github.com/rockchip-linux/mpp.git"
-MPP_VERSION = for_linux
+MPP_VERSION = release
 MPP_CONF_OPTS = "-DRKPLATFORM=ON"
 MPP_CONF_DEPENDENCIES += libdrm
 
