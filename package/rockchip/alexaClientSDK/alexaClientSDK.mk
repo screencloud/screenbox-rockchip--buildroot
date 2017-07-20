6f96d250851c8fc831d44badad7423fb6e827d73
zyh <zyh@rock-chips.com>
2017-07-20 09:00:41 +0800

    rk3036-cpp: alexa: update configs
    Change-Id: Iaa360d69037afee6616905c8b9353417cbae4f24
    Signed-off-by: zyh <zyh@rock-chips.com>

--- package/rockchip/alexaClientSDK/alexaClientSDK.mk
+++ package/rockchip/alexaClientSDK/alexaClientSDK.mk
@@ -5,7 +5,7 @@ ifeq ($(BR2_PACKAGE_ALEXACLIENTSDK),y)
 ALEXACLIENTSDK_SITE = $(TOPDIR)/../external/alexaClientSDK
 ALEXACLIENTSDK_SITE_METHOD = local
 ALEXACLIENTSDK_INSTALL_STAGING = YES
-
+ALEXACLIENTSDK_DEPENDENCIES = libcurl libnghttp2 openblas gst1-plugins-base gst1-plugins-ugly portaudio
 ALEXACLIENTSDK_CONF_OPTS +=\
 						   CMAKE_CURRENT_SOURCE_DIR= source \
 						   -DLIBRARY_OUTPUT_PATH=$(TOPDIR)/output/target/usr/lib/ \
