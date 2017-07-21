f0cbcea403d8d6c35827618dce9f61b03e7c5947
zyh <zyh@rock-chips.com>
2017-07-21 09:48:43 +0800

    rk3036: alexa: change openblas to cblas & remove libmsc.so
    Change-Id: I8d32c4c92bb1963afedbe122f58544a4ea9fdc94
    Signed-off-by: zyh <zyh@rock-chips.com>

--- package/rockchip/alexaClientSDK/alexaClientSDK.mk
+++ package/rockchip/alexaClientSDK/alexaClientSDK.mk
@@ -5,16 +5,19 @@ ifeq ($(BR2_PACKAGE_ALEXACLIENTSDK),y)
 ALEXACLIENTSDK_SITE = $(TOPDIR)/../external/alexaClientSDK
 ALEXACLIENTSDK_SITE_METHOD = local
 ALEXACLIENTSDK_INSTALL_STAGING = YES
-ALEXACLIENTSDK_DEPENDENCIES = libcurl libnghttp2 openblas gst1-plugins-base gst1-plugins-ugly portaudio
+ALEXACLIENTSDK_DEPENDENCIES = libcurl libnghttp2 lapack clapack  gst1-plugins-base gst1-plugins-ugly portaudio
 ALEXACLIENTSDK_CONF_OPTS +=\
 						   CMAKE_CURRENT_SOURCE_DIR= source \
 						   -DLIBRARY_OUTPUT_PATH=$(TOPDIR)/output/target/usr/lib/ \
 						   -DEXECUTABLE_OUTPUT_PATH=$(TOPDIR)/output/target/usr/bin \
 						   -DCMAKE_BUILD_TYPE=DEBUG \
+						   -DGSTREAMER_MEDIA_PLAYER=ON
+
+ALEXACLIENTSDK_CONF_OPTS +=\
 						   -DKITTAI_KEY_WORD_DETECTOR=ON \
 						   -DKITTAI_KEY_WORD_DETECTOR_LIB_PATH=$(TOPDIR)/output/build/alexaClientSDK/source/snowboy/lib/rpi/libsnowboy-detect.a \
 						   -DKITTAI_KEY_WORD_DETECTOR_INCLUDE_DIR=$(TOPDIR)/output/build/alexaClientSDK/source/snowboy/include \
-						   -DGSTREAMER_MEDIA_PLAYER=ON
+
 $(eval $(cmake-package))
 endif
 
