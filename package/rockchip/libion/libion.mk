f8a17be4ae68a30794007bb336fe65ce564daca7
yuyz <yuyz@rock-chips.com>
2017-07-26 11:12:18 +0800

    package/rockchip : update libion to e74d6aa
    release libion with source code
    
    Change-Id: I6e485a5d4e10eaf7efb3cbcbf031a8c03c517384
    Signed-off-by: yuyz <yuyz@rock-chips.com>

--- package/rockchip/libion/libion.mk
+++ package/rockchip/libion/libion.mk
@@ -3,7 +3,7 @@
 
 LIBION_SITE = $(call qstrip, ssh://git@10.10.10.78:2222/argus/externals/libion.git)
 LIBION_SITE_METHOD = git
-LIBION_VERSION = 269603f
+LIBION_VERSION = e74d6aa
 LIBION_INSTALL_STAGING = YES
 LIBION_SOURCE = libion-${LIBION_VERSION}.tar.gz
 LIBION_FROM_GIT = y
