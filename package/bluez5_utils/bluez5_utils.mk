649b7aadb335f8ae9f59ef8713bc0c6fd86542e4
zyh <zyh@rock-chips.com>
2017-08-15 15:50:48 +0800

    rk3036: alexaCPP: modify blueZ to support bt
    Change-Id: Ic4f769de0420c334549990f9ba44e69aa47c8f7a
    Signed-off-by: zyh <zyh@rock-chips.com>

--- package/bluez5_utils/bluez5_utils.mk
+++ package/bluez5_utils/bluez5_utils.mk
@@ -80,6 +80,11 @@ ifeq ($(BR2_PACKAGE_RK3036_ECHO),y)
 BLUEZ5_UTILS_CONF_OPTS += --localstatedir=/data/cfg
 endif
 
+ifeq ($(BR2_PACKAGE_ALEXACLIENTSDK),y)
+BLUEZ5_UTILS_CONF_OPTS += --localstatedir=/data/cfg
+endif
+
+
 define BLUEZ5_UTILS_INSTALL_INIT_SYSTEMD
 	mkdir -p $(TARGET_DIR)/etc/systemd/system/bluetooth.target.wants
 	ln -fs ../../../../usr/lib/systemd/system/bluetooth.service \
