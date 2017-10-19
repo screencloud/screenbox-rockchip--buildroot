29fbdba3571b28615512690e08584a65d04b7027
zyh <zyh@rock-chips.com>
2017-10-19 11:21:07 +0800

    rk322x: softap: fix output path
    Change-Id: Ic3c991c5ce6a0acbf80fbf5074dac2e91967613f
    Signed-off-by: zyh <zyh@rock-chips.com>

--- package/rockchip/softap/softap.mk
+++ package/rockchip/softap/softap.mk
@@ -6,8 +6,8 @@ SOFTAP_SITE = $(TOPDIR)/../external/softapDemo
 SOFTAP_SITE_METHOD = local
 SOFTAP_INSTALL_STAGING = YES
 SOFTAP_CONF_OPTS +=\
-    -DLIBRARY_OUTPUT_PATH=$(TOPDIR)/board/rockchip/rk3036/fs-overlay/usr/lib \
-    -DEXECUTABLE_OUTPUT_PATH=$(TOPDIR)/board/rockchip/rk3036/fs-overlay/usr/bin
+    -DLIBRARY_OUTPUT_PATH=$(TOPDIR)/board/rockchip/rk322x/fs-overlay/usr/lib \
+    -DEXECUTABLE_OUTPUT_PATH=$(TOPDIR)/board/rockchip/rk322x/fs-overlay/usr/bin
 
 $(eval $(cmake-package))
 endif
