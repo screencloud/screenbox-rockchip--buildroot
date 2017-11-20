a346c48387490649c16c94714f69f31125f54534
zyh <zyh@rock-chips.com>
2017-11-20 09:32:00 +0800

    softap: mk: remove unuse path from makefile
    Change-Id: Ida39ce5a3c9c0e9480f29605eef2cc06565d51b1
    Signed-off-by: zyh <zyh@rock-chips.com>

--- package/rockchip/softap/softap.mk
+++ package/rockchip/softap/softap.mk
@@ -5,9 +5,6 @@ ifeq ($(BR2_PACKAGE_SOFTAP), y)
 SOFTAP_SITE = $(TOPDIR)/../external/softapDemo
 SOFTAP_SITE_METHOD = local
 SOFTAP_INSTALL_STAGING = YES
-SOFTAP_CONF_OPTS +=\
-    -DLIBRARY_OUTPUT_PATH=$(TOPDIR)/board/rockchip/rk322x/fs-overlay/usr/lib \
-    -DEXECUTABLE_OUTPUT_PATH=$(TOPDIR)/board/rockchip/rk322x/fs-overlay/usr/bin
 
 $(eval $(cmake-package))
 endif
