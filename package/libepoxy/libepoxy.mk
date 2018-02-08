d8b7dd3cdfa86407cea4a5f2dbad88f20b574690
jkand.huang <jkand.huang@rock-chips.com>
2018-02-08 16:22:48 +0800

    support gtk3.
    Change-Id: I093f52536f057940bdc99035fa52415f99cfa92a
    Signed-off-by: jkand.huang <jkand.huang@rock-chips.com>
    Signed-off-by: james.lin <james.lin@rock-chips.com>

--- package/libepoxy/libepoxy.mk
+++ package/libepoxy/libepoxy.mk
@@ -18,13 +18,16 @@ LIBEPOXY_CONF_OPTS += --enable-egl
 LIBEPOXY_DEPENDENCIES += libegl
 else
 LIBEPOXY_CONF_OPTS += --disable-egl
+#LIBEPOXY_CONF_OPTS += --enable-egl
+#LIBEPOXY_DEPENDENCIES += libegl
 endif
 
 ifeq ($(BR2_PACKAGE_HAS_LIBGL)$(BR2_PACKAGE_XLIB_LIBX11),yy)
 LIBEPOXY_CONF_OPTS += --enable-glx
 LIBEPOXY_DEPENDENCIES += libgl xlib_libX11
 else
-LIBEPOXY_CONF_OPTS += --disable-glx
+LIBEPOXY_CONF_OPTS += --enable-glx
+#LIBEPOXY_CONF_OPTS += --disable-glx
 endif
 
 $(eval $(autotools-package))
