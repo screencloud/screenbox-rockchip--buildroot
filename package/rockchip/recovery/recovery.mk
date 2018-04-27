238f686f289386cca9a06a8432f0617e1b67115a
Eddie Cai <eddie.cai.linux@gmail.com>
2018-04-27 18:29:40 +0800

    package: rockchip: recovery: copy image and pack kernel
    Change-Id: I3fba6a6887a3b5b152ae40b9473e741518d7e0ca
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- package/rockchip/recovery/recovery.mk
+++ package/rockchip/recovery/recovery.mk
@@ -10,9 +10,16 @@ RECOVERY_SITE_METHOD = local
 
 RECOVERY_LICENSE_FILES = NOTICE
 RECOVERY_LICENSE = Apache V2.0
+RECOVERY_KERNEL_DIR = $(TOPDIR)/../kernel
+RECOVERY_KERNEL_IMAGE = $(RECOVERY_KERNEL_DIR)/arch/arm64/boot/Image
+RECOVERY_MK_KERNEL_IMAGE=$(TOPDIR)/../rkbin/tools/mkkrnlimg
+RECOVERY_MKBOOTIMG=$(RECOVERY_KERNEL_DIR)/scripts/mkbootimg
+RECOVERY_RESOURCEIMG=$(RECOVERY_KERNEL_DIR)/resource.img
+RECOVERY_CMDLINNE=buildvariant=userdebug
+RECOVERY_OS_PATCH_LEVEL=2018.04.27
+RECOVERY_OS_VERSION=1.0
 
 RECOVERY_DEPENDENCIES = libdrm libpng
-
 RECOVERY_BUILD_OPTS= \
 	-I$(STAGING_DIR)/usr/include/libdrm \
 	--sysroot=$(STAGING_DIR) \
@@ -37,10 +44,16 @@ define RECOVERY_INSTALL_TARGET_CMDS
         $(INSTALL) -D -m 755 $(@D)/recovery $(TARGET_DIR)/usr/bin/
 endef
 
+ifeq ($(BR2_PACKAGE_RECOVERY),y)
 define RECOVERY_IMAGE_PACK
+	mkdir -p $(TARGET_DIR)/res/images
+	cp $(BUILD_DIR)/recovery-$(RECOVERY_VERSION)/res/images/* $(TARGET_DIR)/res/images/
 	$(HOST_DIR)/usr/bin/mkbootfs $(TARGET_DIR) | $(HOST_DIR)/usr/bin/minigzip > $(BINARIES_DIR)/ramdisk-recovery.img
+	$(RECOVERY_MKBOOTIMG) --kernel $(RECOVERY_MK_KERNEL_IMAGE) --ramdisk $(BINARIES_DIR)/ramdisk-recovery.img --second $(RECOVERY_RESOURCEIMG) --os_version $(RECOVERY_OS_VERSION) --os_patch_level $(RECOVERY_OS_PATCH_LEVEL) --cmdline $(RECOVERY_CMDLINNE) --output $(BINARIES_DIR)/recovery.img
+	$(RECOVERY_MK_KERNEL_IMAGE) $(BINARIES_DIR)/ramdisk-recovery.img $(BINARIES_DIR)/recovery.img
 endef
 
 TARGET_FINALIZE_HOOKS += RECOVERY_IMAGE_PACK
+endif
 
 $(eval $(autotools-package))
