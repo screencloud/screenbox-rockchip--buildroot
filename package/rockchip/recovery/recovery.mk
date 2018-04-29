69fd04dd4a7efa7790ee75e6639f5a129c0ab455
Eddie Cai <eddie.cai.linux@gmail.com>
2018-04-29 17:23:23 +0800

    package: recovery: fix build error
    Change-Id: I4f660d3d1e27257191a15253bbac8f968ea0b190
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- package/rockchip/recovery/recovery.mk
+++ package/rockchip/recovery/recovery.mk
@@ -4,40 +4,27 @@
 #
 ################################################################################
 
-RECOVERY_SITE = $(TOPDIR)/../recovery
 RECOVERY_VERSION = develop
+RECOVERY_SITE = $(TOPDIR)/../recovery
 RECOVERY_SITE_METHOD = local
 
-RECOVERY_LICENSE_FILES = NOTICE
 RECOVERY_LICENSE = Apache V2.0
-RECOVERY_KERNEL_DIR = $(TOPDIR)/../kernel
-RECOVERY_KERNEL_IMAGE = $(RECOVERY_KERNEL_DIR)/arch/arm64/boot/Image
-RECOVERY_MK_KERNEL_IMAGE=$(TOPDIR)/../rkbin/tools/mkkrnlimg
-RECOVERY_MKBOOTIMG=$(RECOVERY_KERNEL_DIR)/scripts/mkbootimg
-RECOVERY_RESOURCEIMG=$(RECOVERY_KERNEL_DIR)/resource.img
-RECOVERY_CMDLINNE=buildvariant=userdebug
-RECOVERY_OS_PATCH_LEVEL=2018.04.27
-RECOVERY_OS_VERSION=1.0
-
-RECOVERY_DEPENDENCIES = libdrm libpng
-RECOVERY_BUILD_OPTS= \
-	-I$(STAGING_DIR)/usr/include/libdrm \
+RECOVERY_LICENSE_FILES = NOTICE
+CC="$(TARGET_CC)"
+PROJECT_DIR="$(@D)"
+RECOVERY_BUILD_OPTS=-I$(PROJECT_DIR) -I$(STAGING_DIR)/usr/include/libdrm \
 	--sysroot=$(STAGING_DIR) \
-	-fPIC
-
-RECOVERY_MAKE = \
-	$(MAKE) BOARD=$(BR2_ARCH) \
+	-fPIC \
+	-lz -lpthread -lpng -ldrm
 
 RECOVERY_MAKE_OPTS = \
-	CC="$(TARGET_CC) $(RECOVERY_BUILD_OPTS)" \
+        CFLAGS="$(TARGET_CFLAGS) $(RECOVERY_BUILD_OPTS)" \
+        PROJECT_DIR="$(@D)"
 
-define RECOVERY_CONFIGURE_CMDS
-	# Do nothing
-endef
+
 
 define RECOVERY_BUILD_CMDS
-	$(TARGET_MAKE_ENV) $(RECOVERY_MAKE) -C $(@D) \
-		$(RECOVERY_MAKE_OPTS)
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) CC="$(TARGET_CC)" $(RECOVERY_MAKE_OPTS)
 endef
 
 define RECOVERY_INSTALL_TARGET_CMDS
@@ -53,7 +40,7 @@ define RECOVERY_IMAGE_PACK
 	$(RECOVERY_MK_KERNEL_IMAGE) $(BINARIES_DIR)/ramdisk-recovery.img $(BINARIES_DIR)/recovery.img
 endef
 
-TARGET_FINALIZE_HOOKS += RECOVERY_IMAGE_PACK
+#TARGET_FINALIZE_HOOKS += RECOVERY_IMAGE_PACK
 endif
 
-$(eval $(autotools-package))
+$(eval $(generic-package))
