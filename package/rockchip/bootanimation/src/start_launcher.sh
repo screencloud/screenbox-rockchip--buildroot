f6fcfe698645077d404a66222be3a9b772fd6be9
sam.lin <lly@rock-chips.com>
2018-05-04 11:51:04 +0800

    bootanimation: 	add S30BootAnimation for display launcher  at boot time 	add start_launcher.sh for play video with mpv
    Change-Id: I16bb920665777ffb8516fe1d6d08315912f30611
    Signed-off-by: sam.lin <lly@rock-chips.com>

---
+++ package/rockchip/bootanimation/src/start_launcher.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+### file: stress_test_dvfs.sh
+### author: xxx@rock-chips.com
+### function: cpu dvfs stress test
+### date: 20180409
+
+echo "**********************stress dvfs test****************************"
+
+url=$1
+echo "url: $url"
+
+#bootid=`ps |grep bootanimation | busybox awk '{ print $2 }'`
+killall  bootanimation
+
+mpv -vo=gpu -gpu-context=drm -hwdec=rkmpp $url
+
+bootanimation &
+
+
+
