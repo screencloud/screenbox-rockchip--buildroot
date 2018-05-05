1ea822f69363039e09d6482b5dbfd2a116312a8e
sam.lin <lly@rock-chips.com>
2018-05-05 18:54:47 +0800

    bootanimation: 	move S30BootAnimation,start_launcher to device/rockchip/rk3128h
    Change-Id: Ieaa84552b0fb84fa8312e005a0ee8f01cd0a5dce
    Signed-off-by: sam.lin <lly@rock-chips.com>

--- package/rockchip/bootanimation/bootanimation.mk
+++ package/rockchip/bootanimation/bootanimation.mk
@@ -5,8 +5,4 @@ BOOTANIMATION_SITE_METHOD = local
 
 BOOTANIMATION_CONF_DEPENDENCIES += libdrm
 
-define BOOTANIMATION_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/S30BootAnimation $(TARGET_DIR)/etc/init.d/S20BootAnimation
-	$(INSTALL) -D -m 0755 $(@D)/start_launcher.sh $(TARGET_DIR)/usr/bin/
-endef
 $(eval $(cmake-package))
