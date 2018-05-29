0a459216f1c17eca4eb7328ec525aeadd227d2e6
Eddie Cai <eddie.cai.linux@gmail.com>
2018-05-29 11:42:54 +0800

    package: rockchip: ueventd: move source code to external/ueventd
    Change-Id: I3d1f7642620f3725f443b6a53509924446258d20
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- package/rockchip/ueventd/ueventd.mk
+++ package/rockchip/ueventd/ueventd.mk
@@ -4,6 +4,13 @@
 #
 ################################################################################
 
+UEVENTD_VERSION = 1.0
+UEVENTD_SITE = $(TOPDIR)/../external/ueventd
+UEVENTD_SITE_METHOD = local
+
+CC="$(TARGET_CC)"
+PROJECT_DIR="$(@D)"
+
 UEVENTD_LICENSE_FILES = NOTICE
 UEVENTD_LICENSE = Apache V2.0
 
@@ -11,17 +18,15 @@ define UEVENTD_TARGET_CFLAGS
 	$(TARGET_CFLAGS) \
 	-D_GNU_SOURCE \
 	-I$(STAGING_DIR)/usr/include \
-	-I$(TOPDIR)/package/rockchip/ueventd/include
+	-I$(PROJECT_DIR)/include
 endef
 
+UEVENTD_MAKE_OPTS = \
+        CFLAGS="$(TARGET_CFLAGS) $(UEVENTD_TARGET_CFLAGS)" \
+        PROJECT_DIR="$(@D)"
+
 define UEVENTD_BUILD_CMDS
-	$(TARGET_CC) $(UEVENTD_TARGET_CFLAGS) $(TARGET_LDFLAGS) \
-		package/rockchip/ueventd/devices.c \
-		package/rockchip/ueventd/util.c \
-		package/rockchip/ueventd/ueventd.c \
-		package/rockchip/ueventd/ueventd_parser.c \
-		package/rockchip/ueventd/parser.c \
-		-o $(@D)/ueventd
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) CC="$(TARGET_CC)" $(UEVENTD_MAKE_OPTS)
 endef
 
 define UEVENTD_INSTALL_TARGET_CMDS
