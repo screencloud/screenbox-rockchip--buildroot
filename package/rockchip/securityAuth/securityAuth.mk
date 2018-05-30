792b2dbab8e3af9bcfb7b9dda1cdec4587be476b
sam.lin <lly@rock-chips.com>
2018-05-30 10:47:16 +0800

    rk3128h:secureAuth :add secureAuth for 3128h
    Change-Id: I35b38601a74b6cff57ce27c452959b524052a53f
    Signed-off-by: sam.lin <lly@rock-chips.com>

--- package/rockchip/securityAuth/securityAuth.mk
+++ package/rockchip/securityAuth/securityAuth.mk
@@ -20,11 +20,11 @@ define SECURITYAUTH_BUILD_CMDS
 	mkdir -p $(TARGET_DIR)/usr/bin
 	mkdir -p $(TARGET_DIR)/usr/lib
 
-	$(INSTALL) -D -m 0644 $(@D)/$(CHIP_NAME)/optee_armtz/* $(TARGET_DIR)/lib/optee_armtz/
+	$(INSTALL) -D -m 0666 $(@D)/$(CHIP_NAME)/optee_armtz/* $(TARGET_DIR)/lib/optee_armtz/
 
-	$(INSTALL) -D -m 0644 $(@D)/$(CHIP_NAME)/bin/* $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -D -m 0755 $(@D)/$(CHIP_NAME)/bin/* $(TARGET_DIR)/usr/bin/
 
-	$(INSTALL) -D -m 0644 $(@D)/$(CHIP_NAME)/lib/* $(TARGET_DIR)/usr/lib/
+	$(INSTALL) -D -m 0666 $(@D)/$(CHIP_NAME)/lib/* $(TARGET_DIR)/usr/lib/
 endef
 endif
 
